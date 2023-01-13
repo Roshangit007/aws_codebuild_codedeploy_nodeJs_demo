d773cdd35d6db212b98530de8c8aa1bf332593d0
Roshan <63492990+Roshangit007@users.noreply.github.com>
2023-01-13 17:32:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('CICD For NODE APP using CodePipeline!')
+  res.send('CICD For NODE APP using CodePipeline updated 13th jan!')
 })
 
 app.get('/status', (req, res) => {
